70c51b79593596d0c9d7c191bfd351a8473db40e
mpatsiou <batsioularas@gmail.com>
2020-12-17 22:04:48 +0200

    The bot ask for user's data

---
+++ bot.py
@@ -0,0 +1,27 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot(
+    'Mirka',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch']
+)
+
+trainer = ListTrainer(bot)
+trainer.train([
+    'The prediction is.....',
+])
+
+print("Welcome to the Bot Service!")
+name = input("Mirka: What's your name? ")
+an = input('Mirka: Please enter your age, gender, weight and height\n' + name + ':').split(' ')
+print(type(name))
+while True:
+    request = input('Mirka: Do you want a prediction?\n'+ name + ':');
+    if request == 'No' or request == 'no':
+        print('Mirka: Ok, bye')
+        break
+
+    else:
+        response = bot.get_response(request)
+        print('Mirka:',response)
